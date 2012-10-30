47aece62b2507f775038bb60d9532c29a3273fb8
Uli Köhler <ulikoehler@online.de>
2012-10-30 04:07:38 +0100

    Move to front works now

---
+++ ebwt/mtf.hpp
@@ -0,0 +1,68 @@
+/* 
+ * File:   mtf.hpp
+ * Author: uli
+ *
+ * Created on 30. Oktober 2012, 02:59
+ */
+
+#ifndef MTF_HPP
+#define	MTF_HPP
+#include <deque>
+using namespace std;
+
+deque<char> getAlphabet(const char* str, size_t len) {
+    //Set all elements to true that occur in str
+    bool occurrence[256];
+    for (int i = 0; i < 256; i++) {
+        occurrence[i] = false;
+    }
+    //Iterate over str
+    for (int i = 0; i < len; i++) {
+        occurrence[(uint8_t) str[i]] = true;
+    }
+    //Build the alphabet deque
+    deque<char> ret;
+    for (int i = 0; i < 256; i++) {
+        if (occurrence[i]) {
+            char val = i;
+            ret.push_back(val);
+        }
+    }
+    return ret;
+}
+
+string alphabetToString(const deque<char>& alphabet) {
+    string ret;
+    for (char c : alphabet) {
+        ret += c;
+    }
+    return ret;
+}
+
+deque<char> alphabetFromString(const string& s) {
+    deque<char> ret;
+    for(char c : s) {
+        ret.push_back(c);
+    }
+    return ret;
+}
+
+/**
+ * In-Place move to front encoder
+ * @param str
+ * @param len
+ */
+void moveToFrontEncode(char* str, size_t len, deque<char>& alphabet) {
+    for (int i = 0; i < len; i++) {
+        const char val = str[i];
+        auto it = find(alphabet.begin(), alphabet.end(), val);
+        uint8_t index = it - alphabet.begin();
+        //Set the index
+        str[i] = index; //Alphabet can be at most 256 characters --> no need for multi-byte indices
+        //Move the index to the front
+        alphabet.erase(it);
+        alphabet.push_front(val);
+    }
+}
+#endif	/* MTF_HPP */
+
--- ebwt/test/BWTTest.cpp
+++ ebwt/test/BWTTest.cpp
@@ -5,15 +5,45 @@
 #include <iostream>
 #include <map>
 #include <boost/algorithm/string.hpp>
+#include "../mtf.hpp"
 
 using namespace std;
 
 BOOST_AUTO_TEST_CASE(TestInfilenameProcessing) {
     string infile = "/home/uli/test.txt";
-    infile.erase(0,infile.find_last_of('/')+1);
+    infile.erase(0, infile.find_last_of('/') + 1);
     BOOST_CHECK_EQUAL("test.txt", infile);
     //Test without slash
     infile = "test.txt";
-    infile.erase(0,infile.find_last_of('/')+1);
+    infile.erase(0, infile.find_last_of('/') + 1);
     BOOST_CHECK_EQUAL("test.txt", infile);
+}
+
+BOOST_AUTO_TEST_CASE(TestGetAlphabet) {
+    string test = "ATATATATGCCGAG"; //ATGC
+    deque<char> alphabet = getAlphabet(test.c_str(), test.length());
+    BOOST_CHECK_EQUAL(4, alphabet.size());
+    BOOST_CHECK(alphabet.end() != find(alphabet.begin(), alphabet.end(), 'A'));
+    BOOST_CHECK(alphabet.end() != find(alphabet.begin(), alphabet.end(), 'G'));
+    BOOST_CHECK(alphabet.end() != find(alphabet.begin(), alphabet.end(), 'C'));
+    BOOST_CHECK(alphabet.end() != find(alphabet.begin(), alphabet.end(), 'T'));
+    //Negative result expected
+    BOOST_CHECK(alphabet.end() == find(alphabet.begin(), alphabet.end(), 'F'));
+}
+
+BOOST_AUTO_TEST_CASE(TestMTF) {
+    const char* testConst = "bananaaa"; //ATGC
+    char* test = new char[strlen(testConst)];
+    memcpy(test, testConst, strlen(testConst) + 1);
+    deque<char> alphabet = alphabetFromString("abcdefghijklmnopqrstuvwxyz");
+    moveToFrontEncode(test, strlen(test), alphabet); //In-place
+    //BAD!! std::string modification in-place
+    BOOST_CHECK_EQUAL(1, test[0]);
+    BOOST_CHECK_EQUAL(1, test[1]);
+    BOOST_CHECK_EQUAL(13, test[2]);
+    BOOST_CHECK_EQUAL(1, test[3]);
+    BOOST_CHECK_EQUAL(1, test[4]);
+    BOOST_CHECK_EQUAL(1, test[5]);
+    BOOST_CHECK_EQUAL(0, test[6]);
+    BOOST_CHECK_EQUAL(0, test[7]);
 }
