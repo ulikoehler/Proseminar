25a286d27ad722454e74cce0e4ff3fd4d4065c41
Uli Köhler <ulikoehler@online.de>
2012-11-07 13:05:28 +0100

    Added LZ4

--- util/pdb.sh
+++ util/pdb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This script downloads 200 random PDB files for the bioinformatics compression corpus
 wget -qO- www.rcsb.org/pdb/rest/getCurrent | grep structureId | cut -d\" -f2 > pdbids
-for i in $(cat pdbids | sort -R | head -n 200) ; do echo ${i} && wget http://www.rcsb.org/pdb/files/${i}.pdb.gz ; done
+for i in $(cat pdbids | sort -R | head -n 50) ; do echo ${i} && wget http://www.rcsb.org/pdb/files/${i}.pdb.gz ; done
 tar cvf Bioinf-200pdb *.pdb
 rm *.pdb
 for i in $(cat pdbids | sort -R | head -n 2500) ; do echo ${i} && wget "http://www.rcsb.org/pdb/download/downloadFile.do?fileFormat=FASTA&compression=NO&structureId="${i} -O ${i}.fa ; done
