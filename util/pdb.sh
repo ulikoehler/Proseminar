cd30979dba33a717bd18cbd9dd3a0384c7e998d4
Uli Köhler <ulikoehler@online.de>
2012-11-07 04:18:10 +0100

    Implemented more data generators

--- util/pdb.sh
+++ util/pdb.sh
@@ -3,3 +3,7 @@
 wget -qO- www.rcsb.org/pdb/rest/getCurrent | grep structureId | cut -d\" -f2 > pdbids
 for i in $(cat pdbids | sort -R | head -n 200) ; do echo ${i} && wget http://www.rcsb.org/pdb/files/${i}.pdb.gz ; done
 tar cvf Bioinf-200pdb *.pdb
+rm *.pdb
+for i in $(cat pdbids | sort -R | head -n 2500) ; do echo ${i} && wget "http://www.rcsb.org/pdb/download/downloadFile.do?fileFormat=FASTA&compression=NO&structureId="${i} -O ${i}.fa ; done
+tar cvf Bioinf-protseq *.fa
+rm pdbids
