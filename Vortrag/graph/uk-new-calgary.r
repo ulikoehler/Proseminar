2ac063ee37fa02bedee99aa5cb2084ce1407329d
Uli Köhler <ulikoehler@online.de>
2012-11-10 21:15:45 +0100

    Improved diagram clarity

--- Vortrag/graph/uk-new-calgary.r
+++ Vortrag/graph/uk-new-calgary.r
@@ -1,12 +1,44 @@
 df1 <- as.data.frame(read.csv(file="../../rawdata/ebwt.stats-book1.txt",head=TRUE,sep=","))
 
-#delete <- c("")
+dfOriginal <- df1
+
+countCharOccurrences <- function(char, s) {
+  s2 <- gsub(char,"",s)
+  return (nchar(s2) - nchar(s))
+}
 
 df1 <- df1[df1$Algorithm!="BWT+MTF",]
 #df1 <- df1[df1$Algorithm!="BWT+MTF",]
 df1 <- df1[df1$Algorithm!="MTF",]
+df1 <- df1[df1$Algorithm!="MTF+Snappy",]
+df1 <- df1[df1$Algorithm!="MTF+LZO",]
+df1 <- df1[df1$Algorithm!="MTF+LZ4",]
+df1 <- df1[df1$Algorithm!="MTF+Huffman",]
+
+df1 <- df1[df1$Algorithm!="RLE",]
+df1 <- df1[df1$Algorithm!="RLE+Snappy",]
+df1 <- df1[df1$Algorithm!="RLE+LZO",]
+df1 <- df1[df1$Algorithm!="RLE+LZ4",]
+
+df1 <- df1[df1$Algorithm!="RLE+Huffman",]
+
+df1 <- df1[df1$Algorithm!="BWT+RLE+Snappy",]
+df1 <- df1[df1$Algorithm!="BWT+RLE+LZO",]
+df1 <- df1[df1$Algorithm!="BWT+RLE+LZ4",]
+
+df1 <- df1[df1$Algorithm!="BWT+MTF+Snappy",]
+df1 <- df1[df1$Algorithm!="BWT+MTF+LZO",]
+df1 <- df1[df1$Algorithm!="BWT+MTF+LZ4",]
+
+df1 <- df1[df1$Algorithm!="BWT+LZO",]
+df1 <- df1[df1$Algorithm!="BWT+LZ4",]
+df1 <- df1[df1$Algorithm!="BWT+Snappy",]
+
+df1 <- df1[df1$Algorithm!="RLE",]
 df1 <- df1[df1$Group!="EncodingOnly",]
-df1 <- df1[df1$Group!="CompressionOnly",]
+df1 <- df1[df1$Algorithm!="LZO",]
+df1 <- df1[df1$Algorithm!="LZ4",]
+df1 <- df1[df1$Algorithm!="Huffman",]
 
 library(ggplot2,quiet=TRUE)
 
