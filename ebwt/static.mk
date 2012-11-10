c21ea10af44a7c4ed00a56a583ec8e3232653860
Uli Köhler <ulikoehler@online.de>
2012-11-10 18:35:05 +0100

    Added libhuffman code

--- ebwt/static.mk
+++ ebwt/static.mk
@@ -1,2 +1,2 @@
 all:
-	g++ -pipe -o bwt -O2 -march=corei7 -ffast-math -std=c++0x -static main.cpp ../huffman/huffman.c ../lz4/lz4.c -ffunction-sections -fdata-sections -Wl,--gc-sections -lboost_thread -lboost_system -pthread -lboost_program_options -lsnappy -llzo2 -s
+	g++ -pipe -o bwt -O2 -march=corei7 -ffast-math -std=c++0x -static main.cpp ../huffman/huffman.c ../lz4/lz4.c -ffunction-sections -fdata-sections -Wl,--gc-sections -lboost_thread -lboost_system -pthread -lboost_program_options -lsnappy -llzo2 -ltcmalloc -s
